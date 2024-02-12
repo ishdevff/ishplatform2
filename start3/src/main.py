c5fbad393bf1b2e7c7d084ddcfb4fe1f857e644d
maxim317 <66307830+maxim317@users.noreply.github.com>
2024-02-12 17:25:53 +0300

    Update main.py

--- start3/src/main.py
+++ start3/src/main.py
@@ -24,7 +24,7 @@ def main(context):
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World! By maxim@medvedkov.me ISHDEVFF")
+        return context.res.send("Hello, World! By maxim@medvedkov.me2 ISHDEVFF")
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
