eb894b2225908a4a98434af7bad00765beb2d351
VALPARFENOV <valery.v.parfenov@yandex.ru>
2024-02-17 16:55:38 +0300

    -

--- addFormProject/src/main.js
+++ addFormProject/src/main.js
@@ -4,7 +4,7 @@ import { Client,Databases } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   try {
       if (req.method === 'GET') {
-          return res.send('Function was updated'); //yyjjjllll
+          return res.send('Function was updated'); //y
       }
 
       if (req.method === 'POST') {
